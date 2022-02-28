648ea7c2cb05da2ce0fb25450db01e79228b7870
Warren Goldman <Warren.Goldman@pacelabs.com>
2022-02-28 13:56:08 -0600

    Fixed dates to show for previous thursday and next thursday

--- src/App.js
+++ src/App.js
@@ -2,9 +2,41 @@ import './App.css';
 import Card from './components/UI/Card';
 import TeeTimes from './components/TeeTimes/TeeTimes';
 
+function formatDate(d) {
+  let month = '' + (d.getMonth() + 1);
+  let day = '' + d.getDate();
+  let year = d.getFullYear();
+  if (month.length < 2) 
+      month = '0' + month;
+  if (day.length < 2) 
+      day = '0' + day;
+
+  return [month, day, year].join('-');
+}
+function getPreviousThursdayDate() {
+  let date = new Date();
+  let difference =  date.getDay() - 4;
+  if (difference < 0 ) 
+  {
+     difference = 7 - (-1*difference);
+  }
+  date.setDate( date.getDate() - difference );
+  return date;
+}
+function getNextThursdayDate() {
+  let date = new Date();
+  let difference =  4 - date.getDay(); 
+  if (difference < 0 ) 
+  {
+     difference = 7 + difference;
+  }
+  date.setDate( date.getDate() + difference );
+  return date;
+}
 function App() {
-  const comingThursday = '03-27-2022';
-  const previousThursday = '03-24-2022';
+  
+  const previousThursday = formatDate(getPreviousThursdayDate());
+  const comingThursday = formatDate(getNextThursdayDate());
   return (
     <Card>
       <h3>Golf Tee Time(s) For {comingThursday}</h3>
