203959918fcad33bd156ab5c8465f351a516c906
Warren Goldman <Warren.Goldman@pacelabs.com>
2022-03-11 16:20:52 -0600

    more formatting

--- src/components/TeeTimes/TeeTime.js
+++ src/components/TeeTimes/TeeTime.js
@@ -10,7 +10,7 @@ const TeeTime = (props) => {
         <div className={classes.time}>
           Tee time: {teeTime} Number:{props.teeNbr}
         </div>
-        <p className={classes.golfername}>{names}</p>
+        <div className={classes.golfername}>{names}</div>
       </div>
     </li>
   );
